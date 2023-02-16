d2312c3c98b458910fb6b47f0fb54fb7f005b2f4
EliaMagris <elia.magris@libero.it>
2023-02-16 12:11:04 +0100

    images db modificato

--- database/migrations/2023_02_15_155638_create_images_table.php
+++ database/migrations/2023_02_15_155638_create_images_table.php
@@ -17,7 +17,6 @@ class CreateImagesTable extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('img');
-            $table->string('cover');
             $table->timestamps();
 
         });
