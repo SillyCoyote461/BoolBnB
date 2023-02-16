5e4418036caa60a0489af28b97433ff2d272c402
EliaMagris <elia.magris@libero.it>
2023-02-16 11:23:57 +0100

    aggiornamento forein key

---
+++ database/migrations/2023_02_16_102051_add_fk_apartment_to_views_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFkApartmentToViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('views', function (Blueprint $table) {
+            $table->unsignedBigInteger('fk_apartment');
+            $table->foreign('fk_apartment')->references('id')->on('apartments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('views', function (Blueprint $table) {
+            //
+        });
+    }
+}
