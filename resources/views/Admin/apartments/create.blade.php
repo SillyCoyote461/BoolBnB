6891986c7d05fbf86e5931f4df9a0f95f79db304
Francesco C <fran461j@outlook.it>
2023-02-17 19:46:46 +0100

    create, edit modified

--- resources/views/Admin/apartments/create.blade.php
+++ resources/views/Admin/apartments/create.blade.php
@@ -2,80 +2,89 @@
 @section('content')
 
 
-<div class="container">
-    <h1>CREATE</h1>
+<div class="container my-5">
+    <h1>Aggiungi inserzione</h1>
 
-    <form class="container" action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
+    <form class="container my-4" action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         {{-- name --}}
         <div>
-            <label class="form-label" for="">Nome</label>
-            <input class="form-control" type="text" name="name">
-        </div>
-        {{-- rooms --}}
-        <div>
-            <label class="form-label" for="">Stanze</label>
-            <input class="form-control" type="number" name="rooms">
-        </div>
-        {{-- beds --}}
-         <div>
-            <label class="form-label" for="">Posti letto</label>
-            <input class="form-control" type="number" name="beds">
-        </div>
-        {{-- baths --}}
-        <div>
-            <label class="form-label" for="">Bagni</label>
-            <input class="form-control" type="number" name="baths">
-        </div>
-        {{-- meters --}}
-        <div>
-            <label class="form-label" for="">Metri quadri</label>
-            <input class="form-control" type="number" name="meters">
-        </div>
-        {{-- address --}}
-        <div>
-            <label class="form-label" for="">Indirizzo</label>
-            <input class="form-control" type="text" name="address">
-        </div>
-        {{-- visibility --}}
-        <div class="form-check form-switch">
-            <input name="visibility" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-            <label class="form-check-label" for="flexSwitchCheckDefault">Visibilitá</label>
+            <label class="form-label" for="name">Titolo</label>
+            <input class="form-control" id="name" type="text" name="name">
         </div>
         {{-- description --}}
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-            <label for="floatingTextarea">Descrizione</label>
+        <div class="form-floating my-3">
+            <textarea class="form-control" id="description"></textarea>
+            <label for="description">Descrizione</label>
         </div>
-        {{-- lat --}}
-        <div>
-            <label class="form-label" for="">Latitudine</label>
-            <input class="form-control" type="text" name="lat">
-        </div>
-        {{-- lon --}}
-        <div>
-            <label class="form-label" for="">Longitudine</label>
-            <input class="form-control" type="text" name="lon">
-        </div>
+        {{-- section apartment --}}
+        <section class="container px-0 d-flex gap-5">
+            {{-- rooms --}}
+            <span>
+                <label class="form-label" for="rooms">Stanze</label>
+                <input value="1" min="1" step="1" type="number" id="rooms" class="form-control" name="rooms" style="width: 100px;"/>
+            </span>
+            {{-- beds --}}
+            <span>
+                <label class="form-label" for="beds">Posti letto</label>
+                <input value="1" min="1" step="1" type="number" id="beds" class="form-control" name="beds"style="width: 100px;"/>
+            </span>
+            {{-- baths --}}
+            <span>
+                <label class="form-label" for="baths">Bagni</label>
+                <input value="1" min="1" step="1" type="number" id="baths" class="form-control" name="baths" style="width: 100px;"/>
+            </span>
+            {{-- meters --}}
+            <span>
+                <label class="form-label" for="meters">Metri quadri</label>
+                <input value="1" min="1" step="1" type="number" id="meters" class="form-control" name="meters" style="width: 100px;"/>
+            </span>
+        </section>
+        {{-- section address --}}
+        <section class="container px-0 mt-3 row">
+            {{-- address --}}
+            <div class="col-5">
+                <label class="form-label" for="address">Indirizzo</label>
+                <input class="form-control" type="text" id="address" name="address">
+            </div>
+            {{-- lat --}}
+            <div class="col-1">
+                <label class="form-label" for="lat">Latitudine</label>
+                <input class="form-control" type="number" id="lat" name="lat">
+            </div>
+            {{-- lon --}}
+            <div class="col-1">
+                <label class="form-label" for="lon">Longitudine</label>
+                <input class="form-control" type="number" id="lon" name="lon">
+            </div>
+        </section>
         {{-- services --}}
-        <div class="my-3">
-            <label for="">Servizi</label>
+        <div class="mt-4">
+            <label>Servizi: </label>
 
             @foreach ($services as $service)
-                <label for="">
-                    <input type="checkbox" name="services[]" value="{{ $service->id }}">
+                <label class="mx-2" for="service{{$service->id}}">
+                    <input type="checkbox" id="service{{$service->id}}" name="services[]" value="{{ $service->id }}">
                     {{ $service->name }}
                 </label>
             @endforeach
 
         </div>
+
         {{-- cover --}}
-        <div class="my-3">
-            <label for="">Aggiungi Cover</label>
-            <input type="file" name="cover" class="form-control-file">
+        <div class="my-3 ">
+            <label for="cover" class="form-label">Aggiungi Cover</label>
+            <input class="form-control" type="file" id="cover" name="cover">
         </div>
+
+        {{-- visibility --}}
+        <div class="form-check form-switch mb-5">
+            <input name="visibility" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+            <label class="form-check-label" for="flexSwitchCheckDefault">Visibilitá</label>
+        </div>
+
         {{-- submit --}}
-        <button class="btn btn-success">Crea inserzione</button>
+        <button class="btn btn-success">Aggiorna inserzione</button>
     </form>
 </div>
 
--- resources/views/Admin/apartments/edit.blade.php
+++ resources/views/Admin/apartments/edit.blade.php
@@ -1,80 +1,89 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="container my-5">
+    <h1>Modifica inserzione</h1>
 
-<div class="container">
-    <h1>edit</h1>
-
-    <form class="container" action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
+    <form class="container my-4" action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         {{-- name --}}
         <div>
-            <label class="form-label" for="">Nome</label>
-            <input class="form-control" type="text" name="name" value="{{$apartment->name}}">
-        </div>
-        {{-- rooms --}}
-        <div>
-            <label class="form-label" for="">Stanze</label>
-            <input class="form-control" type="number" name="rooms" value="{{$apartment->rooms}}">
-        </div>
-        {{-- beds --}}
-         <div>
-            <label class="form-label" for="">Posti letto</label>
-            <input class="form-control" type="number" name="beds" value="{{$apartment->beds}}">
-        </div>
-        {{-- baths --}}
-        <div>
-            <label class="form-label" for="">Bagni</label>
-            <input class="form-control" type="number" name="baths" value="{{$apartment->baths}}">
-        </div>
-        {{-- meters --}}
-        <div>
-            <label class="form-label" for="">Metri quadri</label>
-            <input class="form-control" type="number" name="meters" value="{{$apartment->meters}}">
-        </div>
-        {{-- address --}}
-        <div>
-            <label class="form-label" for="">Indirizzo</label>
-            <input class="form-control" type="text" name="address" value="{{$apartment->address}}">
-        </div>
-        {{-- visibility --}}
-        <div class="form-check form-switch">
-            <input name="visibility" class="form-check-input" {{$apartment->visibility ? 'checked' : ''}} type="checkbox" role="switch" id="flexSwitchCheckDefault">
-            <label class="form-check-label" for="flexSwitchCheckDefault">Visibilitá</label>
+            <label class="form-label" for="name">Titolo</label>
+            <input class="form-control" type="text" id="name" name="name" value="{{$apartment->name}}">
         </div>
         {{-- description --}}
-        <div class="form-floating">
-            <textarea class="form-control" id="floatingTextarea">{{$apartment->description}}</textarea>
-            <label for="floatingTextarea">Descrizione</label>
+        <div class="form-floating my-3">
+            <textarea class="form-control" id="description">{{$apartment->description}}</textarea>
+            <label for="description">Descrizione</label>
         </div>
-        {{-- lat --}}
-        <div>
-            <label class="form-label" for="">Latitudine</label>
-            <input class="form-control" type="text" name="lat" value="{{$apartment->lat}}">
-        </div>
-        {{-- lon --}}
-        <div>
-            <label class="form-label" for="">Longitudine</label>
-            <input class="form-control" type="text" name="lon" value="{{$apartment->lon}}">
-        </div>
+        {{-- section apartment --}}
+        <section class="container px-0 d-flex gap-5">
+            {{-- rooms --}}
+            <span>
+                <label class="form-label" for="rooms">Stanze</label>
+                <input value="1" min="1" step="1" type="number" id="rooms" class="form-control" name="rooms" value="{{$apartment->rooms}}" style="width: 100px;"/>
+            </span>
+            {{-- beds --}}
+            <span>
+                <label class="form-label" for="beds">Posti letto</label>
+                <input value="1" min="1" step="1" type="number" id="beds" class="form-control" name="beds" value="{{$apartment->beds}}" style="width: 100px;"/>
+            </span>
+            {{-- baths --}}
+            <span>
+                <label class="form-label" for="baths">Bagni</label>
+                <input value="1" min="1" step="1" type="number" id="baths" class="form-control" name="baths" value="{{$apartment->baths}}" style="width: 100px;"/>
+            </span>
+            {{-- meters --}}
+            <span>
+                <label class="form-label" for="meters">Metri quadri</label>
+                <input value="1" min="1" step="1" type="number" id="meters" class="form-control" name="meters" value="{{$apartment->meters}}" style="width: 100px;"/>
+            </span>
+        </section>
+        {{-- section address --}}
+        <section class="container px-0 mt-3 row">
+            {{-- address --}}
+            <div class="col-5">
+                <label class="form-label" for="">Indirizzo</label>
+                <input class="form-control" type="text" name="address" value="{{$apartment->address}}">
+            </div>
+            {{-- lat --}}
+            <div class="col-1">
+                <label class="form-label" for="">Latitudine</label>
+                <input class="form-control" type="text" name="lat" value="{{$apartment->lat}}">
+            </div>
+            {{-- lon --}}
+            <div class="col-1">
+                <label class="form-label" for="">Longitudine</label>
+                <input class="form-control" type="text" name="lon" value="{{$apartment->lon}}">
+            </div>
+        </section>
         {{-- services --}}
-        <div class="my-3">
-            <label>Servizi</label>
+        <div class="mt-3">
+            <label>Servizi: </label>
 
             @foreach ($services as $service)
-                <label>
-                    <input type="checkbox {{$apartment->services->contains($service) ? 'checked' : ''}}" name="services[]" value="{{ $service->id }}">
+                <label class="mx-2" for="service{{$service->id}}">
+                    <input type="checkbox" id="service{{$service->id}}" name="services[]" value="{{ $service->id }}" {{$apartment->services->contains($service) ? 'checked' : ''}}>
                     {{ $service->name }}
                 </label>
             @endforeach
 
         </div>
+
         {{-- cover --}}
-        <div class="my-3">
-            <label for="">Cambia Cover</label>
-            <input type="file" name="cover" class="form-control-file">
+        <div class="my-3 ">
+            <label for="cover" class="form-label">Aggiungi Cover</label>
+            <input class="form-control" type="file" id="cover" name="cover">
         </div>
+
+        {{-- visibility --}}
+        <div class="form-check form-switch mb-5">
+            <input name="visibility" class="form-check-input" type="checkbox" role="switch" id="visibility" {{$apartment->visibility ? 'checked' : ''}}>
+            <label class="form-check-label" for="visibility">Visibilitá</label>
+        </div>
+
         {{-- submit --}}
         <button class="btn btn-success">Aggiorna inserzione</button>
     </form>
 </div>
+
