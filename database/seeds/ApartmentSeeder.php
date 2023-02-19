e8b0fde90c14454ef95f2f10582f6664cf73f62c
EliaMagris <elia.magris@libero.it>
2023-02-19 10:55:02 +0100

    faker inizializzato

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\apartment;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ApartmentSeeder extends Seeder
 {
@@ -9,8 +10,22 @@ class ApartmentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $newPost = new Apartment();
+            $newPost->title = $faker->name();
+            $newPost->rooms = $faker->between(1, 10);
+            $newPost->beds = $faker->between(1, 7);
+            $newPost->baths = $faker->between(1, 3);
+            $newPost->meters = $faker->between(30, 100);
+            $newPost->address = $faker->address();
+            $newPost->visibility = $faker->boolean();
+            $newPost->description = $faker->words(30, true);
+            $newPost->cover = $faker->abstract(1234, 2345);
+            $newPost->lat = $faker->latitude();
+            $newPost->lon = $faker->number(5);
+            $newPost->save();
+        }
     }
 }
--- resources/js/views/pages/IndexVue.vue
+++ resources/js/views/pages/IndexVue.vue
@@ -1,99 +1,104 @@
 <template>
-  <div>
-    <!-- JUMBO -->
+    <div>
+        <!-- JUMBO -->
 
-    <div class="backgroundImg d-flex flex-column">
-      <div class="d-flex align-items-center justify-content-center flex-grow-1">
-        <div class="">
-          <form class="form-inline d-flex">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-light border-3 purple my-2 my-sm-0" type="submit">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </form>
+        <div class="backgroundImg d-flex flex-column">
+            <div
+                class="d-flex align-items-center justify-content-center flex-grow-1"
+            >
+                <div class="">
+                    <form class="form-inline d-flex">
+                        <input
+                            class="form-control mr-sm-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button
+                            class="btn btn-outline-light border-3 purple my-2 my-sm-0"
+                            type="submit"
+                        >
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="">Filtri</div>
+            </div>
         </div>
-      </div>
-      <div class="d-flex justify-content-center">
-        <div class="">Filtri</div>
-      </div>
+
+        <!-- SPONSORED / APPARTAMENTI CICLATI -->
+
+        <section>
+            <div class="containerCustom py-5">
+                <!-- SPONSORED -->
+
+                <div
+                    class="border-bottom border-3 p-3 overflow-x-auto grid gap-3"
+                >
+                    <h1>In Primo Piano</h1>
+                    <div>
+                        <div class="card" style="width: 18rem">
+                            <img src="" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Some quick example text to build on the card
+                                    title and make up the bulk of the card's
+                                    content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- APPARTAMENTI CICLATI -->
+
+                <div class="p-3 border-3 d-flex flex-wrap grid gap-3">
+                    <div class="card" style="width: 18rem">
+                        <img
+                            src="../../../../public/img/jumbo.jpg"
+                            class="card-img-top"
+                            alt="..."
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-
-    <!-- SPONSORED / APPARTAMENTI CICLATI -->
-
-    <section>
-      <div class="containerCustom py-5">
-
-        <!-- SPONSORED -->
-
-        <div class="border-bottom border-3 p-3 overflow-x-auto grid gap-3">
-          <h1>In Primo Piano</h1>
-          <div>
-
-            <div v-for="" class="card" style="width: 18rem">
-              <img src="" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-        <!-- APPARTAMENTI CICLATI -->
-
-        <div class="p-3 border-3 d-flex flex-wrap grid gap-3">
-
-          <div class="card " style="width: 18rem">
-            <img src="../../../../public/img/jumbo.jpg" class="card-img-top " alt="..." />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </section>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "IndexVue",
+    name: "IndexVue",
 };
-</script>
-
 </script>
 
 <style lang="scss" scoped>
 .backgroundImg {
-  border-bottom: 5px solid black;
-  background-image: url("../../../../public/img/jumbo.jpg");
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  width: 100%;
-  height: 57rem;
+    border-bottom: 5px solid black;
+    background-image: url("../../../../public/img/jumbo.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 57rem;
 }
 
 .containerCustom {
-  margin: auto;
-  width: 80%;
+    margin: auto;
+    width: 80%;
 }
 
 .purple {
     color: var(--bs-purple);
     border-color: var(--bs-purple);
 }
-
 </style>
