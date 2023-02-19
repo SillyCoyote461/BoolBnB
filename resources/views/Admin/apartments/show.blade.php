9ff2275d09b7672ae4170ccbabb8720d8725cb9c
GIGIPR3 <gigiprencipe47@gmail.com>
2023-02-19 22:30:00 +0100

    .

--- resources/views/Admin/apartments/show.blade.php
+++ resources/views/Admin/apartments/show.blade.php
@@ -17,22 +17,40 @@
                     </div>
                 </li>
                 <li>
-                    <span class="title-lista">Beds:</span> {{ $apartment->beds }}
+                    <span class="title-lista">Beds:</span>
+                    <div class="riga ps-3 text-light fw-bold">
+                        {{ $apartment->beds }}
+                    </div>
                 </li>
                 <li>
-                    <span class="title-lista">Baths:</span> {{ $apartment->baths }}
+                    <span class="title-lista">Baths:</span>
+                    <div class="riga ps-3 text-light fw-bold">
+                        {{ $apartment->baths }}
+                    </div>
                 </li>
                 <li>
-                    <span class="title-lista">Meters:</span> {{ $apartment->meters }}
+                    <span class="title-lista">Meters:</span>
+                    <div class="riga ps-3 text-light fw-bold">
+                        {{ $apartment->meters }}
+                    </div>
                 </li>
                 <li>
-                    <span class="title-lista">Price:</span> <span class="fw-bold text-danger">{{ $apartment->price }}</span>
+                    <span class="title-lista">Price:</span>
+                    <div class="riga ps-3 text-light fw-bold">
+                        {{ $apartment->price }} &euro;
+                    </div>
                 </li>
                 <li>
-                    <span class="title-lista">Address</span> <br> {{ $apartment->address }}
+                    <span class="title-lista">Address</span>
+                    <div class="riga ps-3 text-light fw-bold">
+                        {{ $apartment->address }}
+                    </div>
                 </li>
                 <li>
-                    <span class="title-lista">Description:</span> <br> {{ $apartment->description }}
+                    <span class="title-lista">Description:</span>
+                    <div class="riga ps-3 text-light fw-bold">
+                        {{ $apartment->description }}
+                    </div>
                 </li>
             </ul>
 
