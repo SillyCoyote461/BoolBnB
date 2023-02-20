7ba001648b14885abf2f48e7eeb6fe0ff49cac7f
GIGIPR3 <gigiprencipe47@gmail.com>
2023-02-20 09:14:16 +0100

    card rivisitazione

--- resources/views/Admin/apartments/index.blade.php
+++ resources/views/Admin/apartments/index.blade.php
@@ -31,50 +31,51 @@
                 </a>
             </div>
         </section>
-        {{-- apartments--}}
+        {{-- apartments --}}
         <section class="container d-flex row mt-4 mx-auto">
             {{-- title --}}
             <h3 class="col-12">Le mie inserzioni</h3>
 
-            <div class="container pb-5 mt-5 d-flex flex-wrap justify-content-evenly ">
+            <div class="container pb-5 mt-5 d-flex flex-wrap justify-content-evenly">
                 {{-- loop --}}
                 @foreach ($apartments as $item)
-                {{-- card --}}
-                <div class="card" style="width: 18rem;">
-                    {{-- cover --}}
-                    <img src="{{asset("storage/$item->cover")}}" class="card-img-top" alt="...">
-                    {{-- title description --}}
-                    <div class="card-body">
-                      <h5 class="card-title">{{$item->name}}</h5>
-                      <p class="card-text">{{$item->description}}</p>
-                    </div>
-                    {{-- price beds --}}
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">Prezzo per notte: {{$item->price}}</li>
-                      <li class="list-group-item">Posti letto: {{$item->beds}}</li>
-                      {{-- services loop --}}
-                      @if (property_exists('services', $item))
-                      <li class="list-group-item">
-                          Servizi:
-                          @foreach ( $item->services as $item)
-                          <a href="">#{{$item['name']}}</a>
-                          @endforeach
-                        </li>
-                        @endif
-                    </ul>
-                    <div class="card-body d-flex justify-content-between">
-                        <a href="{{route('admin.apartments.show', $item->id)}}" class="card-link"><button class="butt-1"><i class="fa-solid fa-info"></i></button></a>
-                        <a href="{{route('admin.apartments.edit', $item->id)}}" class="card-link"><button class="butt-2"><i class="fa-solid fa-pen-to-square"></i></button></a>
-                        <form method="POST" action="{{route('admin.apartments.destroy', $item->id)}}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </div>
+                    {{-- card --}}
+                    <div class="card">
+                        <div class="card-img">
+                            <img src="{{ asset("storage/$item->cover") }}" class="card-img-top" alt="...">
+                        </div>
+                        <div class="card-info">
+                            <p class="text-title">{{ $item->name }} </p>
+                            <p class="text-body">Posti letto: {{$item->beds}}</p>
+                        </div>
+                        <div class="card-footer">
+                            <span class="text-title bold-violet">&euro;{{ $item->price }}</span>
+                            <div class="d-flex align-items-center justify-content-evenly mt-3">
+                                <div>
+                                    <a href="{{ route('admin.apartments.show', $item->id) }}" class="card-link"><button
+                                            class="butt-1"><i class="fa-solid fa-info"></i></button></a>
+                                </div>
 
-                  </div>
+                                <div>
+                                    <a href="{{ route('admin.apartments.edit', $item->id) }}" class="card-link"><button
+                                            class="butt-2"><i class="fa-solid fa-pen-to-square"></i></button></a>
+                                </div>
+
+                                <div>
+                                    <form method="POST" action="{{ route('admin.apartments.destroy', $item->id) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger"><i
+                                                class="fa-solid fa-trash"></i></button>
+                                    </form>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
                 @endforeach
-                {{-- loop end--}}
+                {{-- loop end --}}
 
             </div>
 
