17115ad25f877ed98cca07e4dc516fd43012029a
EliaMagris <elia.magris@libero.it>
2023-02-20 10:20:04 +0100

    aggiunta tom tom

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,12 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import App from './views/App';
+import tt from '@tomtom-international/web-sdk-maps';
+
+const map = tt.map({
+    key: '<your maps api key>',
+    container: 'map'
+});
 
 const app = new Vue({
     el: '#root',
