79636b225550a5e4410889c42433d64629309d84
EliaMagris <elia.magris@libero.it>
2023-02-23 15:42:06 +0100

    aggiunta lat e lon dinamiche

--- resources/views/Admin/apartments/create.blade.php
+++ resources/views/Admin/apartments/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 @section('content')
 
 
@@ -7,7 +7,7 @@
 
     <form class="container my-4" action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        {{-- name --}}
+        
         <div>
             <label class="form-label" for="name">Titolo *</label>
             <input class="form-control @error('name') is-invalid @enderror" type="text" id="name" name="name"
@@ -18,7 +18,7 @@
         </div>
 
 
-        {{-- description --}}
+        
         <div class="form-floating my-3">
             <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" required ></textarea>
             <label for="description">Descrizione *</label>
@@ -27,9 +27,9 @@
             @enderror
         </div>
 
-        {{-- section apartment --}}
+        
         <section class="container px-0 d-flex gap-5">
-            {{-- rooms --}}
+            
             <span>
                 <label class="form-label" for="rooms">Stanze *</label>
                 <input min="1" step="1" type="number" id="rooms"
@@ -39,7 +39,7 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </span>
-            {{-- beds --}}
+            
             <span>
                 <label class="form-label" for="beds">Posti letto *</label>
                 <input min="1" step="1" type="number" id="beds"
@@ -49,7 +49,7 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </span>
-            {{-- baths --}}
+            
             <span>
                 <label class="form-label" for="baths">Bagni *</label>
                 <input min="1" step="1" type="number" id="baths"
@@ -59,7 +59,7 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </span>
-            {{-- price --}}
+            
             <span>
                 <label class="form-label" for="price">Prezzo *</label>
                 <input min="1" step="0.01" type="number" id="price"
@@ -71,7 +71,7 @@
             </span>
 
 
-            {{-- meters --}}
+            
             <span>
                 <label class="form-label" for="meters">Metri quadri *</label>
                 <input min="1" step="1" type="number" id="meters" class="form-control" name="meters"
@@ -81,9 +81,9 @@
                 @enderror
             </span>
         </section>
-        {{-- section address --}}
+        
         <section class="container px-0 mt-3 row">
-            {{-- address --}}
+            
             <div class="col-5">
                 <label class="form-label" for="address">Indirizzo *</label>
                 <input class="form-control" type="text" name="address"  required>
@@ -91,8 +91,7 @@
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
-            {{-- lat --}}
-            {{-- id="floatingInputDisabled" --}}
+            
 
             <div class="col-2">
                 <label class="form-label" for="lat">Latitudine *</label>
@@ -110,7 +109,7 @@
                 @enderror
             </div>
         </section>
-        {{-- services --}}
+        
         <div class="mt-4">
             <label>Servizi: </label>
 
@@ -123,7 +122,7 @@
 
         </div>
 
-     {{-- cover --}}
+     
      <div class="my-3 ">
         <label for="cover" class="form-label">Aggiungi Cover*</label>
         <input class="form-control" type="file" id="cover" name="cover" required>
@@ -132,16 +131,402 @@
     @enderror
     </div>
 
-        {{-- visibility --}}
+        
         <div class="form-check form-switch mb-5">
             <input name="visibility" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
             <label class="form-check-label" for="flexSwitchCheckDefault">Visibilità</label>
         </div>
 
-        {{-- submit --}}
+       
         <button class="" type="submit">Crea</button>
     </form>
 </div>
 
 
-@endsection
+@endsection --}}
+
+<html>
+
+<head>
+    {{-- Map --}}
+    <link rel="stylesheet" type="text/css" href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps.css" />
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps-web.min.js"></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- Search --}}
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.12/SearchBox-web.js"></script>
+    <link rel="stylesheet" type="text/css"
+        href="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.12/SearchBox.css" />
+
+
+    <link rel="stylesheet" type="text/css"
+        href="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css" />
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js">
+    </script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js">
+    </script>
+
+    {{-- BOot --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+
+    {{-- Font Awsome --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.css'
+        integrity='sha512-+ouAqATs1y4kpPMCHfKHVJwf308zo+tC9dlEYK9rKe7kiP35NiP+Oi35rCFnc16zdvk9aBkDUtEO3tIPl0xN5w=='
+        crossorigin='anonymous' />
+</head>
+
+<body style="">
+    
+    {{-- BOdy --}}
+    <div class="container-fluid w-75 m-auto right-section mt-5 py-5">
+
+        <h1 class="text-center p-4">Add Apartment</h1>
+
+
+    </div>
+
+    
+        
+
+
+
+    <div class="d-flex justify-content-center h-50">
+      <div id="map" style="width: 100%; height: 100%;"></div>
+    </div>
+
+
+
+
+
+
+   
+
+    <form class="d-flex flex-column w-75 m-auto" method="POST" action="{{ route('admin.apartments.store') }}"
+        enctype="multipart/form-data">
+
+        @csrf
+
+        {{-- NAME --}}
+        <div class="mb-4">
+            <label class="form-label">Name</label>
+            <input name="name" type="text" class="form-control ">
+            @error('name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- DESCRIPTION --}}
+        <div class="mb-4">
+            <label class="form-label">Description</label>
+            <textarea name="description" class="form-control"></textarea>
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- IMMAGINE --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Image</label>
+            <input type="file" name="cover" class="form-control-file">
+            @error('cover_image')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- ROOMS --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Rooms</label>
+            <input type="number" min="1" max="50" class="form-control" name="rooms">
+            @error('rooms')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- BEDS --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Beds</label>
+            <input type="number" min="1" max="50" class="form-control" name="beds">
+            @error('beds')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- BATHROOM --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Bathrooms</label>
+            <input type="number" min="1" max="50" class="form-control" name="baths">
+            @error('bathrooms')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- MQ --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Mq</label>
+            <input type="number" min="1" max="1000" class="form-control" name="meters">
+            @error('mq')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- ACCOMODATION --}}
+        {{-- <div class="mb-4">
+            <label class="form-label form-check-label" for="">Accomodation</label>
+            <input type="number" min="1" max="50" class="form-control" name="accomodation">
+            @error('accomodation')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div> --}}
+        
+
+        {{-- ADDRESS --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Address</label>
+            <input type="text" class="form-control" name="address">
+            @error('address')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- PRICE --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Price</label>
+            <input type="number" min="1" class="form-control" name="price">
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- AVAILABLE --}}
+        <div class="mb-4">
+            <label class="form-label form-check-label" for="">Availability</label>
+            <select name="services." class="form-control">
+                <option value=1>Available</option>
+                <option value=0>Not Available</option>
+            </select>
+        </div>
+
+        {{-- SERVICES --}}
+        <div>
+            @foreach ($services as $service)
+                <div class="cat action">
+                    <label>
+                        <input type="checkbox" name="services[]"
+                            value="{{ $service->id }}"><span>{{ $service->name }}</span>
+                    </label>
+                </div>
+            @endforeach
+        </div>
+
+        {{-- INPUT LAT LONG HIDDEN --}}
+        <input  name="lat" type="text" id="lat"> </input>
+        <input  name="lon" type="text" id="long"> </input>
+
+        {{-- INVIO --}}
+        <div>
+            <button type="submit" class="btn btn-primary">Add apartment</button>
+        </div>
+
+    </form>
+    <script>
+        var ITALIA = [12.49427, 41.89056]
+        var map = tt.map({
+            key: "0HdIeR7zDtKAE4DzRGUEAamM4AA7X491",
+            container: "map",
+            center: ITALIA,
+            zoom: 4
+        })
+
+
+
+        var options = {
+            searchOptions: {
+                key: "0HdIeR7zDtKAE4DzRGUEAamM4AA7X491",
+                language: "en-GB",
+                limit: 1,
+            },
+            autocompleteOptions: {
+                key: "0HdIeR7zDtKAE4DzRGUEAamM4AA7X491",
+                language: "en-GB",
+            },
+        }
+        var ttSearchBox = new tt.plugins.SearchBox(tt.services, options)
+        var searchBoxHTML = ttSearchBox.getSearchBoxHTML()
+        var search = document.getElementById(map)
+        document.body.append(searchBoxHTML)
+
+        // ATACCARE SEARCH A MAP
+        var searchMarkersManager = new SearchMarkersManager(map)
+        ttSearchBox.on("tomtom.searchbox.resultsfound", handleResultsFound)
+        ttSearchBox.on("tomtom.searchbox.resultselected", handleResultSelection)
+        ttSearchBox.on("tomtom.searchbox.resultfocused", handleResultSelection)
+        ttSearchBox.on("tomtom.searchbox.resultscleared", handleResultClearing)
+        map.addControl(ttSearchBox, "top-left")
+
+
+
+
+        // EVENT HENDLER
+        function handleResultsFound(event) {
+            var results = event.data.results.fuzzySearch.results
+
+            if (results.length === 0) {
+                searchMarkersManager.clear()
+            }
+            searchMarkersManager.draw(results)
+            fitToViewport(results)
+
+            // Lat
+            var lat
+            var address
+            results.forEach(element => {
+                lat = element.position.lat
+                address = element.address
+            });
+
+            console.log(address)
+            var latitudine = document.getElementById('lat');
+            latitudine.value = lat;
+            // Long
+            var long
+            results.forEach(element => {
+                long = element.position.lng
+            });
+
+            console.log(long)
+            var longitudine = document.getElementById('long');
+            longitudine.value = long;
+        }
+
+        function handleResultSelection(event) {
+            var result = event.data.result
+            if (result.type === "category" || result.type === "brand") {
+                return
+            }
+            searchMarkersManager.draw([result])
+            fitToViewport(result)
+        }
+
+        function fitToViewport(markerData) {
+            if (!markerData || (markerData instanceof Array && !markerData.length)) {
+                return
+            }
+            var bounds = new tt.LngLatBounds()
+            if (markerData instanceof Array) {
+                markerData.forEach(function(marker) {
+                    bounds.extend(getBounds(marker))
+                })
+            } else {
+                bounds.extend(getBounds(markerData))
+            }
+            map.fitBounds(bounds, {
+                padding: 100,
+                linear: true
+            })
+        }
+
+        function getBounds(data) {
+            var btmRight
+            var topLeft
+            if (data.viewport) {
+                btmRight = [
+                    data.viewport.btmRightPoint.lng,
+                    data.viewport.btmRightPoint.lat,
+                ]
+                topLeft = [data.viewport.topLeftPoint.lng, data.viewport.topLeftPoint.lat]
+            }
+            return [btmRight, topLeft]
+        }
+
+        function handleResultClearing() {
+            searchMarkersManager.clear()
+        }
+
+
+        // Manipulation
+        function SearchMarkersManager(map, options) {
+            this.map = map
+            this._options = options || {}
+            this._poiList = undefined
+            this.markers = {}
+        }
+
+        SearchMarkersManager.prototype.draw = function(poiList) {
+            this._poiList = poiList
+            this.clear()
+            this._poiList.forEach(function(poi) {
+                var markerId = poi.id
+                var poiOpts = {
+                    name: poi.poi ? poi.poi.name : undefined,
+                    address: poi.address ? poi.address.freeformAddress : "",
+                    distance: poi.dist,
+                    classification: poi.poi ? poi.poi.classifications[0].code : undefined,
+                    position: poi.position,
+                    entryPoints: poi.entryPoints,
+                }
+                var marker = new SearchMarker(poiOpts, this._options)
+                marker.addTo(this.map)
+                this.markers[markerId] = marker
+            }, this)
+        }
+
+        SearchMarkersManager.prototype.clear = function() {
+            for (var markerId in this.markers) {
+                var marker = this.markers[markerId]
+                marker.remove()
+            }
+            this.markers = {}
+            this._lastClickedMarker = null
+        }
+
+
+
+
+        // -------------
+        function SearchMarker(poiData, options) {
+            this.poiData = poiData
+            this.options = options || {}
+            this.marker = new tt.Marker({
+                element: this.createMarker(),
+                anchor: "bottom",
+            })
+            var lon = this.poiData.position.lng || this.poiData.position.lon
+            this.marker.setLngLat([lon, this.poiData.position.lat])
+        }
+
+        SearchMarker.prototype.addTo = function(map) {
+            this.marker.addTo(map)
+            this._map = map
+            return this
+        }
+
+        SearchMarker.prototype.createMarker = function() {
+            var elem = document.createElement("div")
+            elem.className = "tt-icon-marker-black tt-search-marker"
+            if (this.options.markerClassName) {
+                elem.className += " " + this.options.markerClassName
+            }
+            var innerElem = document.createElement("div")
+            innerElem.setAttribute(
+                "style",
+                "background: white; width: 10px; height: 10px; border-radius: 50%; border: 3px solid black;"
+            )
+
+            elem.appendChild(innerElem)
+            return elem
+        }
+
+        SearchMarker.prototype.remove = function() {
+            this.marker.remove()
+            this._map = null
+        }
+    </script>
+</body>
+
+</html>
