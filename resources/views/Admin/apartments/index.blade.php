1251ffdbf54f80baf33833be1e32ea60e7728e02
GIGIPR3 <gigiprencipe47@gmail.com>
2023-03-04 16:05:18 +0100

    .

--- resources/views/Admin/apartments/index.blade.php
+++ resources/views/Admin/apartments/index.blade.php
@@ -133,11 +133,91 @@
             </div>
 
 
-            <div class="container pb-5 mt-5 d-flex flex-wrap justify-content-evenly">
+            <div class="container{{--  pb-5 mt-5 --}} d-flex flex-row flex-wrap align-items-center gap-4 {{-- justify-content-evenly --}}">
                 {{-- loop --}}
                 @foreach ($apartments as $item)
                     {{-- card --}}
-                    <div class="card">
+
+                    <div class="card-app" >
+
+                        <div class="immagine-contenitore">
+                            <img src="{{ asset("storage/$item->cover") }}" class="immagine" >
+                        </div>
+
+                        <div class="contenuto">
+                            <div class="titolo">
+                                {{ $item->name }}
+                            </div>
+
+                            <div class="d-flex justify-content-evenly mt-2">
+
+                                <div>
+                                    <a href="{{ route('admin.apartments.show', $item->id) }}" class="card-link"><button
+                                            class="butt-2"><i class="fa-solid fa-info"></i></button></a>
+                                </div>
+
+                                <div>
+                                    <a href="{{ route('admin.apartments.edit', $item->id) }}" class="card-link"><button
+                                            class="butt-2"><i class="fa-solid fa-pen-to-square"></i></button></a>
+                                </div>
+
+                                <div>
+                                    <button type="button" class="bottonefigo" data-bs-toggle="modal"
+                                        data-bs-target="#{{ $item->id }}">
+                                        cancella
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                @endforeach
+
+
+            </div>
+
+        </section>
+
+
+    </div>
+
+
+    <div class="modal fade" id="{{ $item->id }}" tabindex="-1"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+
+                </div>
+                <div class="modal-body">
+                    sei sicuro di voler cancellaere <span
+                        class="fw-bold">{{ $item->name }}</span> ?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="bottonefigo"
+                        data-bs-dismiss="modal">Close</button>
+                    <form method="POST" id="delete-form"
+                        action="{{ route('admin.apartments.destroy', $item->id) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bottonefigo">Cancella</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+                    {{-- <div class="card">
                         <div class="card-img">
                             <img src="{{ asset("storage/$item->cover") }}" class="card-img-top" alt="...">
                         </div>
@@ -192,48 +272,14 @@
                                     </div>
                                 </div>
 
-                                {{-- <div>
-                                    <form method="POST" id="delete-form" action="{{ route('admin.apartments.destroy', $item->id) }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="">Cancella</button>
-                                    </form>
 
-                                </div> --}}
-
-                                <!-- Modale di conferma -->
-                                {{-- <div class="modal" id="confirmDeleteModal" tabindex="-1" role="dialog">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title text-danger">Conferma cancellazione</h5>
-                                            </div>
-                                            <div class="modal-body">
-                                                Sei sicuro di voler cancellare l'appartamento <span class="fw-bold">{{$item->name}}</span>?
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="" data-dismiss="modal">Annulla</button>
-                                                <form method="POST" action="{{ route('admin.apartments.destroy', $item->id) }}">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="">Cancella</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> --}}
 
                             </div>
 
                         </div>
-                    </div>
-                @endforeach
+                    </div> --}}
+
                 {{-- loop end --}}
 
-            </div>
 
-
-
-
-    </div>
 @endsection
